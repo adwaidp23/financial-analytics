559266746625d550788ec69b13a366ce6b4b55e3
adwaidp23 <adwaidp08@gmail.com>
2026-05-27 11:40:10 +0530

    chore: remove dev utility scripts and extracted text files

--- extract_pdf.py
+++
@@ -1,8 +0,0 @@
-import pypdf
-import sys
-
-reader = pypdf.PdfReader(r"d:\workspace\Financial_Analythics\MCA_FA_Capstone_Project.pdf")
-text = "".join([page.extract_text() for page in reader.pages])
-
-with open(r"d:\workspace\Financial_Analythics\MCA_FA_Capstone_Project.txt", "w", encoding="utf-8") as f:
-    f.write(text)
